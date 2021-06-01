0f741e76adef1b5490da84f947734ba8d81f1ddf
monojitwithu <monojitwithu@gmail.com>
2021-06-02 01:56:17 +0530

    First commit Half Done

---
+++ calculator.js
@@ -0,0 +1,233 @@
+class Calculator{    /* this is the class*/
+    constructor(){
+        this.numArr=[]
+        this.currentNum=""
+        this.result=""
+        this.prevResult=""
+        this.operation=[]
+    }
+    add(numOne,numTwo){
+        return Number(numOne)+Number(numTwo)
+    }
+    substract(numOne,numTwo){
+        return Number(numOne)-Number(numTwo)
+        
+    }
+    setCurrentNum(value){
+        this.currentNum+=value
+    }
+    clearCurrentNum(){
+        this.numArr.push(this.currentNum)
+        this.currentNum=""
+    }
+    allClear(){
+        this.numArr=[]
+        this.currentNum=""
+        this.result=""
+        this.prevResult=""
+        this.operation=[]
+
+    }
+    setNumOne(value){
+        this.numOne+=value
+    }
+    setNumTwo(value){
+        this.numTwo+=value
+    }
+    setOperation(operation){
+       this.operation.push(operation)  
+       this.numArr.push(this.currentNum)
+       this.currentNum=""      
+    }
+
+    
+    changeSign(forNumOne){
+        if(forNumOne){
+            this.numArr=substring(Number(this.numArr) * -1)
+            return
+        }
+        this.currentNum=substring(Number(this.currentNum)* -1)
+        
+    }
+    undo(forNumOne){
+        if(forNumOne){
+            this.numArr=this.numArr.substring(0,(this.numArr.length-1))
+            return;
+        }
+        this.currentNum=this.currentNum.substring(0,(this.currentNum.length-1))
+    }
+
+}
+const signObject={
+    ADD:"+"
+    ,
+    SUBSTRACT:"-",
+
+    MULTIPLY:"x"
+}
+
+
+
+
+
+
+
+ 
+
+
+
+const calculator= new Calculator() /*this calculator Object created from Calculator Class and has thouse atributes */
+
+const keyclickhandler =(num)=>{
+    calculator.setCurrentNum(num)
+    updateMainDisplay()
+    updateSecoundarydisplay()
+    }
+
+    const updateMainDisplay = ()=>{
+        const main=document.getElementById("main-display")
+        main.innerText=calculator.currentNum ||0
+    }
+    const updateSecoundarydisplay=()=>{
+        const secoundary=document.getElementById("secoundary-display")
+        const newNumArr=[...calculator.numArr]
+        const newOperation=[...calculator.operation]
+        let innerHTML=""
+        while(newNumArr.length !==0){
+            const num=newNumArr.shift()
+            const operation=newOperation.shift()
+            innerHTML+= `${num}<span class="operand">${signObject[operation]?signObject[operation]:""}</span>`
+        }
+        secoundary.innerHTML=innerHTML
+    }
+    
+
+
+    const operandclickhandler=(operation)=>{
+        calculator.setOperation(operation)
+        console.log(operation)
+        updateMainDisplay()
+        updateSecoundarydisplay()
+
+    }
+    const result=()=>{
+        console.log("working")
+         if(calculator==null){
+            updateMainDisplay()
+             
+             
+         }
+
+
+
+        
+        calculator.clearCurrentNum()
+        
+        const newNumArr=[...calculator.numArr]
+        const newOperation=[...calculator.operation]
+     while(newNumArr.length !== 1){
+        const numOne=newNumArr.shift()
+        const numTwo=newNumArr.shift()
+        const operation=newOperation.shift()
+        
+    switch(operation){
+        case "ADD":
+                newNumArr.unshift(calculator.add(numOne,numTwo))
+                break;
+        case "SUBSTRACT":
+                newNumArr.unshift(calculator.substract(numOne,numTwo))
+                 break;
+            
+
+        default:
+                 break;
+        }
+    }    updateSecoundarydisplay()
+        const main=document.getElementById("main-display")
+        main.innerText=newNumArr[0]
+
+    }
+    
+    
+
+const actionkeyclickhandler=(action)=>{
+    switch(action){
+        case 'ALL_CLEAR':
+            calculator.allClear()
+            break;
+
+
+         case 'CHANGE_SIGN':
+             calculator.changeSign(calculator.operation?false:true)
+             break;
+             
+
+
+    }
+    updateMainDisplay()
+    updateSecoundarydisplay()
+    }
+
+
+    const undoclickhandler=()=>{
+        
+        calculator.undo(calculator.operation?false:true)
+        updateMainDisplay()
+        
+
+    }
+
+
+
+
+    function Check(){
+        console.log("working")
+    }
+//need to add more functions
+
+    
+    
+       
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
